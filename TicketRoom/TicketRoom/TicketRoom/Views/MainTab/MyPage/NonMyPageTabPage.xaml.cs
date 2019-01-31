7d1e7ec2cd17ba569c31f209900cbfbf38229c3d
MarkPC <dhdldhdl45@gmail.com>
2019-01-31 15:00:02 +0900

    1. Views/MainTab/MyPage/NonMyPageTabPage.xml 비 로그인 시 나오는 마이페이지 2019-01-31

---
+++ TicketRoom/TicketRoom/TicketRoom/Views/MainTab/MyPage/NonMyPageTabPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using TicketRoom.Views.Users.Login;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TicketRoom.Views.MainTab.MyPage
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class NonMyPageTabPage : ContentView
+	{
+		public NonMyPageTabPage ()
+		{
+			InitializeComponent ();
+		}
+
+        private void LoginCreate_Clicked(object sender, EventArgs e)
+        {
+            //로그인 페이지 이동
+            Navigation.PushModalAsync(new LoginPage());
+        }
+    }
+}
