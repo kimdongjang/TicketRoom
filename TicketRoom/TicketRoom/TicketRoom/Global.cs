8827eca909dad81db8f85cc0f5551162d8f7daf2
DESKTOP-ALJ8QIP\LDH <1147725@naver.com>
2019-01-31 16:25:26 +0900

    2019-01-31 병합전

--- TicketRoom/TicketRoom/TicketRoom/Global.cs
+++ TicketRoom/TicketRoom/TicketRoom/Global.cs
@@ -5,14 +5,14 @@ namespace TicketRoom
 {
     public class Global
     {
-        //public static string WCFURL = @"http://52.231.66.251/Service1.svc/";
+        public static string WCFURL = @"http://52.231.66.251/Service1.svc/";
 
         public static List<G_BasketInfo> BasketList = new List<G_BasketInfo>();
         //public static string WCFURL = @"http://52.231.66.251/Service1.svc/";
         //public static string WCFURL = @"http://220.90.190.218/Service1.svc/";
         //public static string WCFURL = @"http://localhost:65192/Service1.svc/";
         //운기 로컬 Services
-        public static string WCFURL = @"http://220.90.190.218:8081/Service1.svc/";
+        //public static string WCFURL = @"http://220.90.190.218:8081/Service1.svc/";
 
         public static string ID = "1147725";
 
