ebd485b1fe3622465777991261c5b6bfe83c9896
DESKTOP-ALJ8QIP\LDH <1147725@naver.com>
2019-02-21 12:00:15 +0900

    2019-02-21 메인 페이지 백버튼 종료여부 확인 병합전

---
+++ TicketRoom/TicketRoom/TicketRoom.Android/Renderer/CloseApplication.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using TicketRoom.Droid.Renderer;
+using TicketRoom.Models.Custom;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(CloseApplication))]
+namespace TicketRoom.Droid.Renderer
+{
+    public class CloseApplication : ICloseApplication
+    {
+        public void closeApplication()
+        {
+            Android.OS.Process.KillProcess(Android.OS.Process.MyPid());
+            //var activity = (Activity)Forms.Context;
+            //activity.FinishAffinity();
+        }
+    }
+}
