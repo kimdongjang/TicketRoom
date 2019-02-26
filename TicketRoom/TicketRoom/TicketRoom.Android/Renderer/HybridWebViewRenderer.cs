acb721981b1a32aeca6606eb1b229f879cb288b9
DESKTOP-ISTUFMH\Print <naru_oo@naver.com>
2019-02-26 18:56:04 +0900

    2019-02-26 PG 결제 연동 테스트 완료

---
+++ TicketRoom/TicketRoom/TicketRoom.Android/Renderer/HybridWebViewRenderer.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Webkit;
+using Android.Widget;
+using Java.Interop;
+using TicketRoom.Droid.Renderer;
+using TicketRoom.Models;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(HybridWebView), typeof(HybridWebViewRenderer))]
+namespace TicketRoom.Droid.Renderer
+{
+    public class HybridWebViewRenderer : ViewRenderer<HybridWebView, Android.Webkit.WebView>
+    {
+        const string JavascriptFunction = "function invokeCSharpAction(data){jsBridge.invokeAction(data);}";
+        Context _context;
+
+        public HybridWebViewRenderer(Context context) : base(context)
+        {
+            _context = context;
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<HybridWebView> e)
+        {
+            base.OnElementChanged(e);
+
+            if (Control == null)
+            {
+                var webView = new Android.Webkit.WebView(_context);
+                webView.Settings.JavaScriptEnabled = true;
+                webView.SetWebViewClient(new JavascriptWebViewClient($"javascript: {JavascriptFunction}"));
+                SetNativeControl(webView);
+            }
+            if (e.OldElement != null)
+            {
+                Control.RemoveJavascriptInterface("jsBridge");
+                var hybridWebView = e.OldElement as HybridWebView;
+                hybridWebView.Cleanup();
+            }
+            if (e.NewElement != null)
+            {
+                Control.AddJavascriptInterface(new JSBridge(this), "jsBridge");
+                Control.LoadUrl($"file:///android_asset/{Element.Uri}");
+            }
+        }
+    }
+    public class JSBridge : Java.Lang.Object
+    {
+        readonly WeakReference<HybridWebViewRenderer> hybridWebViewRenderer;
+
+        public JSBridge(HybridWebViewRenderer hybridRenderer)
+        {
+            hybridWebViewRenderer = new WeakReference<HybridWebViewRenderer>(hybridRenderer);
+        }
+
+        [JavascriptInterface]
+        [Export("invokeAction")]
+        public void InvokeAction(string data)
+        {
+            HybridWebViewRenderer hybridRenderer;
+
+            if (hybridWebViewRenderer != null && hybridWebViewRenderer.TryGetTarget(out hybridRenderer))
+            {
+                hybridRenderer.Element.InvokeAction(data);
+            }
+        }
+    }
+    public class JavascriptWebViewClient : WebViewClient
+    {
+        string _javascript;
+
+        public JavascriptWebViewClient(string javascript)
+        {
+            _javascript = javascript;
+        }
+
+        public override void OnPageFinished(Android.Webkit.WebView view, string url)
+        {
+            base.OnPageFinished(view, url);
+            view.EvaluateJavascript(_javascript, null);
+        }
+    }
+}
